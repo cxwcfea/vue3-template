5e5a8c66d1990ff8861f17a29eed0493ecd645c8
chengxiang <xiang.cheng@meritco-group.com>
2023-11-16 23:45:38 +0800

    style: add unocss and less config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
+
+import '@/style/reset.css';
+import '@/style/index.less';
+import 'uno.css';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import viteEslint from 'vite-plugin-eslint';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import UnoCSS from 'unocss/vite';
 import path from 'path';
+import presetUno from '@unocss/preset-uno';
+import presetAttributify from '@unocss/preset-attributify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +15,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [vue(), viteEslint(), vueJsx()],
+  plugins: [
+    vue(),
+    viteEslint(),
+    vueJsx(),
+    UnoCSS({
+      presets: [presetUno(), presetAttributify()],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `
+          @import "@/style/variables.less";
+        `,
+      },
+    },
+  },
 });
