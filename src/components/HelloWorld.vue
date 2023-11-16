f8e9b91c2345226ec3dd54f4eb8861b50dbb34a5
chengxiang <xiang.cheng@meritco-group.com>
2023-11-17 00:19:53 +0800

    build: use element-plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,15 @@
+<template>
+  <h1 text-gray-500>{{ msg }}</h1>
+
+  <div class="card">
+    <el-button type="primary" @click="count++">count is {{ count }}</el-button>
+    <p class="read-the-docs">
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue';
 
@@ -5,18 +17,6 @@ defineProps<{ msg: string }>();
 
 const count = ref(0);
 </script>
-
-<template>
-  <h1 text-gray-500>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p class="read-the-docs">
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-</template>
 
 <style lang="less" scoped>
 .read-the-docs {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import viteEslint from 'vite-plugin-eslint';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import UnoCSS from 'unocss/vite';
-import path from 'path';
 import presetUno from '@unocss/preset-uno';
 import presetAttributify from '@unocss/preset-attributify';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +24,12 @@ export default defineConfig({
     vueJsx(),
     UnoCSS({
       presets: [presetUno(), presetAttributify()],
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   css: {
